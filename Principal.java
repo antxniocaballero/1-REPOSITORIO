22c64864d499fa7b3d9d79adb51bead9df24e2ff
antxniocaballero <antoniocaballero@ejemplo.com>
2026-01-30 11:52:24 +0100

    Paso 11: Añadida funcionalidad de fecha

--- Principal.java
+++ Principal.java
@@ -1,5 +1,12 @@
+import java.time.LocalDateTime;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        mostrarFecha();
+    }
+
+    public static void mostrarFecha() {
+        System.out.println("Fecha y hora: " + LocalDateTime.now());
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Proyecto de Programación
 Este repositorio contiene las prácticas de clase en Java.
+## Funcionalidad Practica4_1
+Se ha implementado un método para mostrar la fecha y hora actual.
